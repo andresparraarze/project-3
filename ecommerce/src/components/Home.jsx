7c531c4e704326d215e600d83b9d834b7f4a5b92
Parra <andresparraarze@gmail.com>
2022-07-30 00:02:25 -0400

    Front end basic

--- ecommerce/src/components/Home.jsx
+++ ecommerce/src/components/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import image1 from '../assets/largemain1.jpg'
+import image2 from '../assets/largemain2.jpg'
+import image3 from '../assets/largemain3.jpg'
 
 const Home = () => {
   return (
     <div name='home' className='w-full h-screen'>
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center items-center h-full'>
-            <img style={{width: '400px'}} src="https://media1.popsugar-assets.com/files/thumbor/a43ui40l-2C7ifyuzC63ISFrc50/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2022/02/01/787/n/1922564/cab87ce4a13756f5_BALENCIAGA_CAMPAIGN_KIM_KARDASHIAN_HOURGLASS_NO_LOGO/i/Kim-Kardashian-For-Balenciaga.jpg" alt="Home img" />
+        <div className='max-w-[1600px] mx-auto px-8 flex flex-col justify-center items-center h-full select-none'>
+            <img className='aspectw-16 aspect-h-9'
+              style={{width: '1600px'}} src={image1} alt='home'/>
+
+            <div className='absolute top-1/2 transform -translate-y-1/2 px-3 flex justify-between items-center text-black font-bolder'>
+              <button className=''>
+                Previous
+              </button>
+              <button className=''>
+                Next
+              </button>
+            </div>
         </div>
     </div>
   )
--- ecommerce/src/components/Navbar.jsx
+++ ecommerce/src/components/Navbar.jsx
@@ -1,5 +1,11 @@
-import React, {useState} from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import React, {useState, Fragment} from 'react'
+import {FaBars, FaTimes, FaShoppingCart} from 'react-icons/fa'
+import {Menu, Transition} from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 const Navbar = () => {
 
@@ -11,14 +17,133 @@ const Navbar = () => {
     <div className='fixed w-full h-[90px] flex justify-between items-center px-4 bg-[#19191b] text-white'>
       
       {/*Logo*/}
-      <div className='font-extrabold text-2xl'>
-        <h1>LEA LACROIX</h1>
+      <div className='font-extrabold text-2xl cursor-pointer'>
+        <a href="/">LEA LACROIX</a>
       </div>
 
       {/*reamining nav bar icons*/}
       <ul className='hidden md:flex font-bold'>
         <li>
-          Categories
+          <Menu as="div" className="relative inline-block">
+            <div>
+              <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-bold text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-yellow-600">
+                Categories
+                <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+              </Menu.Button>
+            </div>
+
+            <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+      >
+            <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none">
+              <div className="py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="/"
+                      className={classNames(
+                        active ? 'bg-gray-200 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      Menu
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="/"
+                      className={classNames(
+                        active ? 'bg-gray-200 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      Women
+                    </a>
+                  )}
+                </Menu.Item>
+              </div>
+              <div className="py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="/"
+                      className={classNames(
+                        active ? 'bg-gray-200 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      New Season
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="/"
+                      className={classNames(
+                        active ? 'bg-gray-200 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      Accessories
+                    </a>
+                  )}
+                </Menu.Item>
+              </div>
+              <div className="py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="/"
+                      className={classNames(
+                        active ? 'bg-gray-200 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      Vintage
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="/"
+                      className={classNames(
+                        active ? 'bg-gray-200 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      Old Season
+                    </a>
+                  )}
+                </Menu.Item>
+              </div>
+              <div className="py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="/"
+                      className={classNames(
+                        active ? 'bg-gray-200 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm'
+                      )}
+                    >
+                      Special Edition
+                    </a>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+            </Transition>
+          </Menu>
         </li>
         <li>
           Shopping cart
@@ -32,26 +157,27 @@ const Navbar = () => {
       </ul>
 
       {/*mobile navbar*/}
+      <div className='md:hidden z-10'>
+        <FaShoppingCart/>
+      </div>
+
       <div onClick={handleClick} className='md:hidden z-10'>
         {!nav ? <FaBars/> : <FaTimes/> }
       </div>
-
+      
       {/*Mobile menu*/}
-      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[##010101] font-bold flex flex-col justify-center items-center text-white'}>
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[##5e5e5e] font-bold flex flex-col justify-center items-center text-white'}>
         <li className='py-6 text-4xl'>
           Home
         </li>
         <li className='py-6 text-4xl'>
-          Shop
+          Categories
         </li>
         <li className='py-6 text-4xl'>
           About
           </li>
         <li className='py-6 text-4xl'>
           Login
-        </li>
-        <li className='py-6 text-4xl'>
-          Signup
         </li>
       </ul>
     </div>
