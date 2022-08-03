4f9ef7860faabb20056b14eb9e5e3ee06c362b2c
Evelyn Torres <evelyn@Evelyns-MacBook-Air.local>
2022-08-02 17:47:34 -0700

    adding routes and seller and user path to app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,7 @@ import { setContext } from '@apollo/client/link/context';
 import {Route, Routes} from 'react-router-dom';
 
 const httpLink = createHttpLink({
-  uri:  '/graphql'
+  uri:  'http://localhost:3001/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
